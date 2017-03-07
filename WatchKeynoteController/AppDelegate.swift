e2d60b6784deae7aee4a5dabfd60140e890e8040
ezefranca <ezequiel.ifsp@gmail.com>
2017-03-06 22:02:11 -0300

    Initial Commit

---
+++ WatchKeynoteController/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  WatchKeynoteController
+//
+//  Created by Ezequiel on 06/03/17.
+//  Copyright © 2017 Ezequiel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
